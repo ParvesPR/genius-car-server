d5ddd0cece30cee4a96e2ccd3a9ec27455814ad5
ParvesPR <parves2585@gmail.com>
2022-04-24 12:49:56 +0600

    connect with mongodb

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // MIDDLEWARE
 app.use(cors());
 app.use(express.json());
+
+// CONNECT TO MONGODB
+
+const uri = "mongodb+srv://dbuser:<password>@cluster0.z9ugs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
 
 app.get('/', (req, res) => {
     res.send('Genius car server running')
